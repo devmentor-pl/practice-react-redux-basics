104ee8e7a8d57f76c565005ef4e29a20f77a42af
AnnaBuller <aniabela@wp.pl>
2021-02-01 15:14:28 +0100

    task 02 solved

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { createStore } from 'redux';
+import {Provider} from 'react-redux';
 
 import Task01 from './../../01/Task01';
 import Task02 from './../../02/Task02';
@@ -6,17 +8,20 @@ import Task03 from './../../03/Task03';
 import Task04 from './../../04/Task04';
 import Task05 from './../../05/Task05';
 
+const store = createStore(() => ({message: 'Działa!'}));
 
 const App = () => {
     return (
-        <>
-            <Task01 />
-            {/* <Task02 /> */}
-            {/* <Task03 /> */}
-            {/* <Task04 /> */}
-            {/* <Task05 /> */}
-        </>
-    )
+		<>
+			{/* <Task01 /> */}
+            <Provider store={store}>
+			    <Task02 />
+            </Provider>
+			{/* <Task03 /> */}
+			{/* <Task04 /> */}
+			{/* <Task05 /> */}
+		</>
+	);
 }
 
 export default App;
