da35307c9f40c90f3c8f969dda45c6676af8ace6
AnnaBuller <aniabela@wp.pl>
2021-02-01 15:05:23 +0100

    task 01 solved

--- 01/Task01.js
+++ 01/Task01.js
@@ -1,45 +1,49 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Subject from './../src/libs/Subject';
 
 const Task01 = () => {
     const [time, setTime] = useState(0);
     const [tagName, setTagName] = useState('??');
     const [cursorPosition, setCursorPosition] = useState('[?,?]');
-
+    const subject = new Subject();
+    
     const setEventTime = ({timeStamp}) => {
         setTime( parseInt(timeStamp / 1000) );
     }
-
+    
     const setEventTagName = ({target}) => {
         setTagName(target.tagName);
     }
-
+    
     const setEventCursorPosition = ({pageX, pageY}) => {
         setCursorPosition(`[${pageX},${pageY}]`);
     }
+    
+    const taskFuncs = [setEventTime, setEventTagName, setEventCursorPosition];
+	useEffect(() => subscribeToSubject(taskFuncs));
+
+	function subscribeToSubject(taskFuncs) {
+		taskFuncs.forEach((func) => subject.subscribe(func));
+	}
 
     return (
-        <section>
-            <h1>Task 1</h1>
-            
-            <div onClick={ event => {
-                setEventTime(event);
-                setEventTagName(event);
-                setEventCursorPosition(event);
-            }}>
-                <p>
-                    <strong>Kliknij wybrany element:</strong> <a>link</a>, <button>button</button>, <span>span</span>
-                </p>
+		<section>
+			<h1>Task 1</h1>
 
+			<div onClick={(event) => subject.notify(event)}>
+				<p>
+					<strong>Kliknij wybrany element:</strong> <a>link</a>,{' '}
+					<button>button</button>, <span>span</span>
+				</p>
 
-                <ul>
-                    <li>time: { time }s</li>
-                    <li>tagName: { tagName }</li>
-                    <li>position: { cursorPosition }</li>
-                </ul>
-            </div>
-        </section>
-    )
+				<ul>
+					<li>time: {time}s</li>
+					<li>tagName: {tagName}</li>
+					<li>position: {cursorPosition}</li>
+				</ul>
+			</div>
+		</section>
+	);
 }
 
 export default Task01;
