cf11fb4e82f0251beb13f9d139e459fbdbb87fa8
AnnaBuller <aniabela@wp.pl>
2021-02-02 14:33:55 +0100

    move store and reducer to their new files

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createStore } from 'redux';
+import store from '../store'
 import {Provider} from 'react-redux';
 
 import Task01 from './../../01/Task01';
@@ -7,37 +7,6 @@ import Task02 from './../../02/Task02';
 import Task03 from './../../03/Task03';
 import Task04 from './../../04/Task04';
 import Task05 from './../../05/Task05';
-
-const initState = {
-    message: 'Działa!',
-    time: new Date(),
-    users: [],
-}
-const reducer = (state = initState, action) => {
-    switch(action.type) {
-        case 'getCurrentTime':
-            return {
-                ...state,
-                time: new Date()
-            }
-        case 'addUser':
-            return {
-                ...state,
-                users: [...state.users, action.payload.newUser]
-            }
-        case 'removeUser':
-            return {
-				...state,
-				users: state.users.filter((user) => user.id !== action.payload),
-			};
-        default: return state
-    }
-};
-
-const store = createStore(reducer, 
-    window.__REDUX_DEVTOOLS_EXTENSION__ 
-    && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
 
 const App = () => {
     return (
@@ -47,8 +16,8 @@ const App = () => {
 			    <Task02 />
 			    <Task03 />
 			    <Task04 />
+			<Task05 />
             </Provider>
-			{/* <Task05 /> */}
 		</>
 	);
 }
