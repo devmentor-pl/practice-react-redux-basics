f200fcc2245745e2f12fac4c6ee39e65d2cd036a
AnnaBuller <aniabela@wp.pl>
2021-02-02 14:32:52 +0100

    create new files

---
+++ src/actions.js
@@ -0,0 +1,19 @@
+export function getCurrentTime() {
+	return {
+		type: 'getCurrentTime',
+	};
+}
+
+export function addUser(newUser) {
+	return {
+		type: 'addUser',
+		payload: { newUser: newUser },
+	};
+}
+
+export function removeUser(id) {
+	return {
+		type: 'removeUser',
+		payload: id,
+	};
+}
