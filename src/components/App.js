73d2b95e952b6edcd1dad8e1684630ee4fd6d3fd
mlvrkhn <martin.guide@gmail.com>
2021-02-14 15:26:34 +0100

    Solve task 3

--- src/components/App.js
+++ src/components/App.js
@@ -9,15 +9,34 @@ import Task04 from './../../04/Task04';
 import Task05 from './../../05/Task05';
 
 const App = () => {
-    const store = createStore(() => {
-        return { message: 'dziala!' };
-    });
+    const initialState = { message: 'dziala!', time: new Date() };
+
+    const updateTime = () => {
+        console.log('pd');
+        return { time: '21:37' };
+    };
+
+    const reducer = (state = initialState, action) => {
+        switch (action.type) {
+            case 'getCurrentTime':
+                console.log('yey');
+                return { ...state, time: new Date() };
+            // updateTime() // mogę to obsłuyć poza reduktorem?
+            default:
+                return state;
+        }
+    };
+    const store = createStore(
+        reducer,
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+            window.__REDUX_DEVTOOLS_EXTENSION__()
+    );
 
     return (
         <Provider store={store}>
             <Task01 />
             <Task02 />
-            {/* <Task03 /> */}
+            <Task03 />
             {/* <Task04 /> */}
             {/* <Task05 /> */}
         </Provider>
