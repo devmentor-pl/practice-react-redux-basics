daad9eca9bf6891c1e2c20df34e2b967ed7e6b43
mlvrkhn <martin.guide@gmail.com>
2021-02-14 13:50:06 +0100

    Solve task 2

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 import Task01 from './../../01/Task01';
 import Task02 from './../../02/Task02';
@@ -6,17 +8,20 @@ import Task03 from './../../03/Task03';
 import Task04 from './../../04/Task04';
 import Task05 from './../../05/Task05';
 
-
 const App = () => {
+    const store = createStore(() => {
+        return { message: 'dziala!' };
+    });
+
     return (
-        <>
+        <Provider store={store}>
             <Task01 />
-            {/* <Task02 /> */}
+            <Task02 />
             {/* <Task03 /> */}
             {/* <Task04 /> */}
             {/* <Task05 /> */}
-        </>
-    )
-}
+        </Provider>
+    );
+};
 
 export default App;
