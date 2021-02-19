f3970b19e7f075dc7d375517c57de90ee7858b59
Mateusz Sowa <mateusz.jan.sowa@gmail.com>
2021-02-19 14:00:23 +0100

    Task 02 ready

--- src/app.js
+++ src/app.js
@@ -1,9 +1,18 @@
 // ./src/app.js
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+const store = createStore(() => {
+  return { message: 'Działa!' };
+});
 
 import App from './components/App';
 
 ReactDOM.render(
-  <App />, document.querySelector('#root')
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
 );
