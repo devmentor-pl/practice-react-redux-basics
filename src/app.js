e8b17ea115d111d862a9c90941b6f4c15270db4b
gregor232 <ktotok@gmail.com>
2021-05-27 08:30:08 +0200

    task 02 finished

--- src/app.js
+++ src/app.js
@@ -1,9 +1,22 @@
 // ./src/app.js
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
-import App from './components/App';
+import App from "./components/App";
+
+const store = createStore(() => {
+  return {
+    message: "Działa",
+  };
+});
 
 ReactDOM.render(
-  <App />, document.querySelector('#root')
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.querySelector("#root")
 );
