bc8aa33a8f18463873a606675a57b6dc73c48254
Rafał Kazik <kazik.raf@gmail.com>
2022-03-27 20:15:52 +0200

    2nd task

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 import Task01 from './../../01/Task01';
 import Task02 from './../../02/Task02';
@@ -6,17 +8,25 @@ import Task03 from './../../03/Task03';
 import Task04 from './../../04/Task04';
 import Task05 from './../../05/Task05';
 
-
 const App = () => {
-    return (
-        <>
-            <Task01 />
-            {/* <Task02 /> */}
-            {/* <Task03 /> */}
-            {/* <Task04 /> */}
-            {/* <Task05 /> */}
-        </>
-    )
-}
+  const store = createStore(() => {
+    return { message: 'Działa!' };
+  });
+
+  return (
+    <>
+      <Task01 />
+      <React.StrictMode>
+        <Provider store={store}>
+          <Task02 />
+        </Provider>
+      </React.StrictMode>
+
+      {/* <Task03 /> */}
+      {/* <Task04 /> */}
+      {/* <Task05 /> */}
+    </>
+  );
+};
 
 export default App;
