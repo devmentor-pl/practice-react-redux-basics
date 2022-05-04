1e03d7af9339c5fb119aee1f9ed405e978725e1d
Ewelina <ewelina@ekopacz.pl>
2022-05-04 21:43:32 +0200

    finish task 01

--- 01/Task01.js
+++ 01/Task01.js
@@ -5,6 +5,7 @@ const Task01 = () => {
     const [time, setTime] = useState(0);
     const [tagName, setTagName] = useState('??');
     const [cursorPosition, setCursorPosition] = useState('[?,?]');
+    const subject = new Subject();
 
     const setEventTime = ({timeStamp}) => {
         setTime( parseInt(timeStamp / 1000) );
@@ -18,15 +19,15 @@ const Task01 = () => {
         setCursorPosition(`[${pageX},${pageY}]`);
     }
 
+    subject.subscribe(setEventTime);
+    subject.subscribe(setEventTagName);
+    subject.subscribe(setEventCursorPosition);
+
     return (
         <section>
             <h1>Task 1</h1>
-            
-            <div onClick={ event => {
-                setEventTime(event);
-                setEventTagName(event);
-                setEventCursorPosition(event);
-            }}>
+
+            <div onClick={ event => {subject.notify(event)}}>
                 <p>
                     <strong>Kliknij wybrany element:</strong> <a>link</a>, <button>button</button>, <span>span</span>
                 </p>
