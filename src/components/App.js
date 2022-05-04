743d8a96bd1b93e6e06ece671eadb7937bba54e8
Ewelina <ewelina@ekopacz.pl>
2022-05-04 22:58:03 +0200

    added code to task 03

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createStore } from 'redux';
 import {Provider} from 'react-redux';
+import reducer from '../reducers';
 
 import Task01 from './../../01/Task01';
 import Task02 from './../../02/Task02';
@@ -9,17 +10,23 @@ import Task04 from './../../04/Task04';
 import Task05 from './../../05/Task05';
 
 
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__
+    && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
 const App = () => {
-    const message = createStore(() =>  ({message:'Działa!'}))
+
     return (
         <>
-            <Task01 />
-            <Provider store={message}>
+            <Provider store={store}>
+                <Task01 />
                 <Task02 />
+                <Task03 />
+                {/* <Task04 /> */}
+                {/* <Task05 /> */}
             </Provider>
-            {/* <Task03 /> */}
-            {/* <Task04 /> */}
-            {/* <Task05 /> */}
         </>
     )
 }
