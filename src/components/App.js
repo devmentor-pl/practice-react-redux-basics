bdb33e3bb5ea8528b29b45f66efcb86da7c88ec7
Ewelina <ewelina@ekopacz.pl>
2022-05-05 17:24:24 +0200

    active Task 05

--- src/components/App.js
+++ src/components/App.js
@@ -25,7 +25,7 @@ const App = () => {
                 <Task02 />
                 <Task03 />
                 <Task04 />
-                {/* <Task05 /> */}
+                <Task05 />
             </Provider>
         </>
     )
