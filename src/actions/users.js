d50faa865dcfb814ef3da96a4810473d84782dfb
Ewelina <ewelina@ekopacz.pl>
2022-05-05 17:09:02 +0200

    added removeUser func

--- src/actions/users.js
+++ src/actions/users.js
@@ -3,14 +3,18 @@ import {v4 as uuid} from 'uuid';
 export const addUser = (value) => {
     return {
         type: 'addUser',
-        payload: { 
+        payload: {
             name: value,
             id: uuid(),
          }
     }
-
 }
 
-export const removeUser = () => {
-
+export const removeUser = (id) => {
+    return {
+        type: 'removeUser',
+        payload: {
+            id: id,
+        }
+    }
 }
