05cbfd73db573f0c499140c7d0900abee09d986b
Ewelina <ewelina@ekopacz.pl>
2022-05-05 16:32:20 +0200

    added solution to the UserList file

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,23 +1,58 @@
 import React from 'react';
 import UserItem from './UserItem';
+import { useSelector} from 'react-redux';
+import {addUser} from '../actions/users'
+import {connect} from 'react-redux';
+
 
 class UserList extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            newUser: '',
+        }
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+        this.props.addNewUser(this.state.newUser);
+        this.setState({
+            newUser: '',
+        })
+    }
+
+    handleChange = e =>{
+        this.setState({
+            [e.target.name]:e.target.value
+        });
+    }
+
     render() {
         return (
             <>
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <div>
-                        <input />
+                        <input onChange={this.handleChange} name="newUser" value={this.state.newUser} />
                         <input type="submit" value="dodaj" />
                     </div>
                 </form>
 
                 <ul>
-                    <UserItem />
+                    {this.props.users.map((user) => <UserItem name={user.name} key={user.id} id={user.id}/>)}
                 </ul>
             </>
         )
     }
 }
 
-export default UserList;
+const mapStateToProps = (state,props) => {
+    return {
+        users: state.users
+    }
+}
+
+const mapActionToProps = {
+    addNewUser: addUser,
+}
+
+export default connect(mapStateToProps,mapActionToProps)(UserList);
