12e4d6f40a9f424bdd7c026966b49f5768cb6b62
Anna <krolikowska.ann@gmail.com>
2022-07-14 19:33:12 +0200

    03 added reducer and redux devtools

--- src/components/App.js
+++ src/components/App.js
@@ -8,18 +8,33 @@ import Task04 from './../../04/Task04';
 import Task05 from './../../05/Task05';
 
 
-const store = createStore(() => {
-    return {message: 'Działa!'}
-})
 
+const initState = {message: 'Działa', time: new Date()}
+
+const reducer = (state = initState, action) => {
+    switch(action.type) {
+        case 'getCurrentTime':
+
+            return {
+               ...state, time: new Date()
+            };
+        default: 
+            return state;
+    }
+}
+
+const store = createStore(reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__
+    && window.__REDUX_DEVTOOLS_EXTENSION__()
+   )
 
 const App = () => {
     return (
         <>
         <Provider store={store}>
-            {/* <Task01 /> */}
+            <Task01 />
             <Task02  />
-            {/* <Task03 /> */}
+            <Task03 />
             {/* <Task04 /> */}
             {/* <Task05 /> */}
         </Provider>
