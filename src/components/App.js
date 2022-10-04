7514bccf05f78ff3fd53a6af6af21a7e80d36f61
Filip <43039909+Urfisior@users.noreply.github.com>
2022-10-05 00:00:07 +0100

    task 03 done

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 import Task01 from './../../01/Task01';
 import Task02 from './../../02/Task02';
@@ -6,15 +8,32 @@ import Task03 from './../../03/Task03';
 import Task04 from './../../04/Task04';
 import Task05 from './../../05/Task05';
 
-
 const App = () => {
+    const initialValue = {
+        message: 'Działa!',
+        time: new Date(),
+    }
+
+    const reducer = (state = initialValue, actions) => {
+        switch (actions.type) {
+            case 'getCurrentTime': {
+                return {...state,time: new Date()}
+            }
+            default:
+                return state;
+        }
+    }
+    const store = createStore(reducer,window.__REDUX_DEVTOOLS_EXTENSION__
+    && window.__REDUX_DEVTOOLS_EXTENSION__());
     return (
         <>
-            <Task01 />
-            {/* <Task02 /> */}
-            {/* <Task03 /> */}
-            {/* <Task04 /> */}
-            {/* <Task05 /> */}
+            <Provider store={store}>
+                <Task01 />
+                <Task02 />
+                <Task03 />
+                {/* <Task04 /> */}
+                {/* <Task05 /> */}
+            </Provider>
         </>
     )
 }
