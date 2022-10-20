b3a8fd7ffb1f8b88dfc5666c7338619dbf305500
Mateusz Bogolubow <mateusz@bogolubow.com>
2022-10-20 15:02:37 +0200

    Finish 02

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,30 @@
 import React from 'react';
 
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
 import Task01 from './../../01/Task01';
 import Task02 from './../../02/Task02';
 import Task03 from './../../03/Task03';
 import Task04 from './../../04/Task04';
 import Task05 from './../../05/Task05';
 
+const store = createStore(() => {
+    return {
+        message: 'Działa!'
+    };
+})
+
 
 const App = () => {
     return (
-        <>
+        <Provider store={store}>
             <Task01 />
-            {/* <Task02 /> */}
+            <Task02 />
             {/* <Task03 /> */}
             {/* <Task04 /> */}
             {/* <Task05 /> */}
-        </>
+        </Provider>
     )
 }
 
