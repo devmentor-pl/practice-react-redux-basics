066c61f3ffaf859c9b106b519275ae92dae62e5d
Mateusz Bogolubow <mateusz@bogolubow.com>
2022-10-20 15:38:18 +0200

    Finish 04

--- src/components/App.js
+++ src/components/App.js
@@ -12,12 +12,20 @@ import Task05 from './../../05/Task05';
 const initState = {
     message: 'Działa!',
     time: new Date(),
+    users: [],
 };
 
 const reducer = (state = initState, action) => {
     switch (action.type) {
         case 'getCurrentTime':
             return { ...state, time: new Date() }
+        case 'addUser':
+            const { data } = action.payload;
+            return { ...state, users: [...state.users, data] }
+        case 'removeUser':
+            const { id } = action.payload;
+            const currUsers = state.users.filter(u => u.id !== id);
+            return { ...state, users: currUsers }
     }
 
     return state
@@ -33,7 +41,7 @@ const App = () => {
             <Task01 />
             <Task02 />
             <Task03 />
-            {/* <Task04 /> */}
+            <Task04 />
             {/* <Task05 /> */}
         </Provider>
     )
