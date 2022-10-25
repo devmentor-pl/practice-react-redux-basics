4fe7e58650b2db1ddabfd9025c524dbc90ce2f94
Mateusz Suplewski <matx3582@gmail.com>
2022-10-25 14:02:58 +0200

    Task-2 Done

--- src/components/App.js
+++ src/components/App.js
@@ -5,13 +5,28 @@ import Task02 from './../../02/Task02';
 import Task03 from './../../03/Task03';
 import Task04 from './../../04/Task04';
 import Task05 from './../../05/Task05';
-
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 
 const App = () => {
+    const initialState = {
+        message: 'Działa!'
+    }
+
+    const reducer = (state = initialState, action) => {
+        switch (action) {
+          default:
+            return state
+        }
+      }
+      
+    const store = createStore(reducer,initialState)
     return (
         <>
             <Task01 />
-            {/* <Task02 /> */}
+            <Provider store={store}>
+              <Task02 />
+            </Provider>
             {/* <Task03 /> */}
             {/* <Task04 /> */}
             {/* <Task05 /> */}
