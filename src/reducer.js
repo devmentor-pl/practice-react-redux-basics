c77dc49fa28f59461a0e73526cdc5cc7f15696d0
Camila Montes Celinski <camicelinski@gmail.com>
2022-10-27 23:03:16 +0200

    Finish task 03

--- src/reducer.js
+++ src/reducer.js
@@ -1,9 +1,12 @@
 const initState = {
   message: 'Działa!',
+  time: new Date(),
 }
 
 const reducer = (state = initState, action) => {
   switch (action.type) {
+    case 'getCurrentTime':
+      return { ...state, time: new Date() }
     default:
       return state
   }
