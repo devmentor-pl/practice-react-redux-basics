748ba5a55851301d858801daa456c66e6e2ef643
Camila Montes Celinski <camicelinski@gmail.com>
2022-10-27 22:50:11 +0200

    Finish task 02

---
+++ src/reducer.js
@@ -0,0 +1,12 @@
+const initState = {
+  message: 'Działa!',
+}
+
+const reducer = (state = initState, action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
+
+export default reducer
