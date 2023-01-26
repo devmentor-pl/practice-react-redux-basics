51939cc63e045c6eefadbd21237ee7034ce6d979
Agnieszka Szostak <agnieszkaszostak94@gmail.com>
2023-01-26 13:36:33 +0100

    Add task2

--- src/components/App.js
+++ src/components/App.js
@@ -5,13 +5,26 @@ import Task02 from './../../02/Task02';
 import Task03 from './../../03/Task03';
 import Task04 from './../../04/Task04';
 import Task05 from './../../05/Task05';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
+const initState = {message: 'Działa!'}
+const reducer = (state = initState, action) => {
+    switch(action.type){
+        default:
+            return state
+    }
+
+}
+const store = createStore(reducer)
 
 const App = () => {
     return (
         <>
-            <Task01 />
-            {/* <Task02 /> */}
+            {/* <Task01 /> */}
+            <Provider store={store}>
+                <Task02/>
+            </Provider>
             {/* <Task03 /> */}
             {/* <Task04 /> */}
             {/* <Task05 /> */}
