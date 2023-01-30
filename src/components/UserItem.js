3443ea52e549bb076d6b4a9fc897d83902b89279
Agnieszka Szostak <agnieszkaszostak94@gmail.com>
2023-01-30 22:57:22 +0100

    Remove useSelector

--- src/components/UserItem.js
+++ src/components/UserItem.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { removeUser } from '../actions';
 
 const UserItem = ({name = 'Default Name', id = -1}) => {
-    const user = useSelector(state => state.users.filter(user => user.id === id)[0]);
     const dispatch = useDispatch();
     
     const handleClick = event => {
         event.preventDefault();
-        dispatch(removeUser(user.id))
+        dispatch(removeUser(id))
     }
     return (
         <li>{name} <button onClick={handleClick}>usuń</button></li>
