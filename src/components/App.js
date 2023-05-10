353d304974fada2cb73d1515b36c1274d95860f0
Michal <maxpoz3212@gmail.com>
2023-05-10 11:13:16 +0200

    Add task03 solution

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 
@@ -8,17 +8,36 @@ import Task03 from "./../../03/Task03";
 import Task04 from "./../../04/Task04";
 import Task05 from "./../../05/Task05";
 
-const store = createStore(() => ({
+const initialState = {
   message: "Działa!",
-}));
+  time: new Date(),
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "getCurrentTime":
+      return {
+        ...state,
+        time: new Date(),
+      };
+
+    default:
+      return state;
+  }
+};
+
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
 
 const App = () => {
   return (
     <Provider store={store}>
       <Task01 />
       <Task02 />
-
-      {/* <Task03 /> */}
+      <Task03 />
       {/* <Task04 /> */}
       {/* <Task05 /> */}
     </Provider>
