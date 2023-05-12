69212814cb746bc732291c6aad59a523a82b610a
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2023-05-12 12:30:21 +0200

    finish task 3

--- src/components/App.js
+++ src/components/App.js
@@ -8,11 +8,27 @@ import Task03 from './../../03/Task03';
 import Task04 from './../../04/Task04';
 import Task05 from './../../05/Task05';
 
-const store = legacy_createStore(() => {
-	return {
-		message: 'Działa!',
-	};
-});
+const INITIAL_STATE = {
+	message: 'Działa!',
+	time: new Date(),
+};
+
+const reducer = (state = INITIAL_STATE, action) => {
+	switch (action.type) {
+		case 'getCurrentTime':
+			return {
+				...state,
+				time: new Date(),
+			};
+		default:
+			return state;
+	}
+};
+
+const store = legacy_createStore(
+	reducer,
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 const App = () => {
 	return (
@@ -20,7 +36,7 @@ const App = () => {
 			<Provider store={store}>
 				<Task01 />
 				<Task02 />
-				{/* <Task03 /> */}
+				<Task03 />
 				{/* <Task04 /> */}
 				{/* <Task05 /> */}
 			</Provider>
