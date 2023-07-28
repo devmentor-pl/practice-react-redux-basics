c83411abc256f5ac89d62c98cde25a0a85d33e65
Diana Rasińska-Kowalczyk <diana.rasinska@gmail.com>
2023-07-28 15:45:08 +0200

    Add ex 3

--- src/components/App.js
+++ src/components/App.js
@@ -8,19 +8,32 @@ import Task03 from "./../../03/Task03";
 import Task04 from "./../../04/Task04";
 import Task05 from "./../../05/Task05";
 
-const store = createStore(() => {
-	return {
-		message: "Działa!",
-	};
-});
+const initialState = {
+	message: "Działa!",
+	time: null,
+};
+
+const reducer = (state = initialState, action) => {
+	switch (action.type) {
+		case "getCurrentTime":
+			return {
+				...state,
+				time: new Date(),
+			};
+		default:
+			return state;
+	}
+};
+
+const store = createStore(reducer);
 
 const App = () => {
 	return (
 		<>
 			<Provider store={store}>
 				{/* <Task01 /> */}
-				<Task02 />
-				{/* <Task03 /> */}
+				{/* <Task02 /> */}
+				<Task03 />
 				{/* <Task04 /> */}
 				{/* <Task05 /> */}
 			</Provider>
