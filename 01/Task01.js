96eeb7ecba18feb403f7853465c1202c701a3bc2
Sebastian <sebastian.pawel.wapinski@gmail.com>
2023-09-26 13:54:04 +0200

    task 1 added

--- 01/Task01.js
+++ 01/Task01.js
@@ -18,14 +18,17 @@ const Task01 = () => {
         setCursorPosition(`[${pageX},${pageY}]`);
     }
 
+    const subject = new Subject()
+    subject.subscribe(setEventTime)
+    subject.subscribe(setEventTagName)
+    subject.subscribe(setEventCursorPosition)
+
     return (
         <section>
             <h1>Task 1</h1>
             
             <div onClick={ event => {
-                setEventTime(event);
-                setEventTagName(event);
-                setEventCursorPosition(event);
+                subject.notify(event)
             }}>
                 <p>
                     <strong>Kliknij wybrany element:</strong> <a>link</a>, <button>button</button>, <span>span</span>
