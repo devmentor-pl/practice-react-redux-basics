c372af9a0b166358d8bb6dee7d883410373ae860
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2023-10-10 13:21:32 +0200

    Finish task 05

--- src/actions/users.js
+++ src/actions/users.js
@@ -11,3 +11,4 @@ export const removeUser = (id) => {
         payload: { id: id }
     }
 }
+
--- src/components/App.js
+++ src/components/App.js
@@ -6,48 +6,10 @@ import Task03 from './../../03/Task03';
 import Task04 from './../../04/Task04';
 import Task05 from './../../05/Task05';
 
-import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import store from '../store/store';
 
 const App = () => {
-
-    const initMessage = { message: "Działa!", time: new Date(), users: [{ id: 0, name: 'Anna' }] }
-    const reducer = (state = initMessage, action) => {
-        switch (action.type) {
-            case 'getCurrentTime':
-                return {
-                    ...state,
-                    time: new Date()
-                }
-            case 'addUser':
-                const { users } = state
-                let id
-                const usersLength = users.length
-
-                if (usersLength !== 0) {
-                    id = users[usersLength - 1].id
-                } else { id = 0 }
-                
-                return {
-                    ...state,
-                    users: [...users, { id: id + 1, name: action.payload.name }]
-                }
-
-            case 'removeUser':
-                const newUsersList = state.users.filter(user => user.id !== action.payload.id)
-
-                return {
-                    ...state,
-                    users: newUsersList
-                }
-            default:
-                return state
-        }
-    }
-    const store = createStore(reducer,
-        window.__REDUX_DEVTOOLS_EXTENSION__
-        && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
 
     return (
 
@@ -56,7 +18,7 @@ const App = () => {
             <Task02 />
             <Task03 />
             <Task04 />
-            {/* <Task05 /> */}
+            <Task05 />
         </Provider>
 
     )
