d8db62566f2d7d6f1a33dc0588cc50ddc33c193f
kozerka <kozerka@gmail.com>
2023-10-10 13:29:53 +0200

    Complete task 03

--- src/components/App.js
+++ src/components/App.js
@@ -8,15 +8,25 @@ import Task03 from './../../03/Task03';
 import Task04 from './../../04/Task04';
 import Task05 from './../../05/Task05';
 
-const store = createStore(() => {
-	return { message: 'Działa!' };
-});
+
+const initialState = { message: 'Działa!', time: new Date() };
+const reducer = (state = initialState, action) => {
+	switch (action.type) {
+		case 'getCurrentTime':
+			return { ...state, time: new Date() };
+	}
+	return state;
+};
+const store = createStore(
+	reducer,
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 const App = () => {
 	return (
 		<Provider store={store}>
 			<Task01 />
 			<Task02 />
-			{/* <Task03 /> */}
+			<Task03 />
 			{/* <Task04 /> */}
 			{/* <Task05 /> */}
 		</Provider>
