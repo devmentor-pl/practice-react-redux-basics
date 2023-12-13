b904840e4d121b8f5d0c50c02592d9513414e9e9
Lukasz <l.bdnski@gmail.com>
2023-12-13 11:20:06 +0100

    add tasks

--- 05/Task05.js
+++ 05/Task05.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import removeUsersAction from '../src/actions/userList';
 
 const Task05 = () => {
+    const dispatch = useDispatch()
     return (
         <section>
             <h1>Task 5</h1>
-            <button>remove all users</button>
+            <button onClick={() => dispatch(removeUsersAction())}>remove all users</button>
         </section>
     );
 }
